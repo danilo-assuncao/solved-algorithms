af3c53fcd76da7329a47a3c8a9750cdcc3eda233
Danilo Assunção <35316786+danilo-assuncao@users.noreply.github.com>
2022-11-15 02:43:01 +0000

    improve the atoi function

--- Atoi.java
+++ Atoi.java
@@ -1,11 +1,25 @@
 class Atoi {
 
     public static void main(String[] args) {
-        myAtoi("45");
+        myAtoi("   4540   ");
     }
 
-    
     public static int myAtoi(String s) {
-        return 0;
+        var number = 0;
+        var signal = '+';
+        var numberCharLength = s.length() - 1;
+        for (int i = numberCharLength; i >= 0; i--) {
+            final var digit = s.charAt(i);
+            if (digit >= 48 && digit <= 57) {
+                number += (digit - '0') * Math.pow(10, numberCharLength - i);
+            } else if (digit == 32) {
+                continue;
+            } else if (digit == 43 || digit == 45) {
+                signal = digit;
+            }
+        }
+        number *= (signal == '+' ? 1 : -1);
+        System.out.println(number);
+        return number;
     }
 }
