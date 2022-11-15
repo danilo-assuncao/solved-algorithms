4ff53b7614692a7b13f0f0e8b32ef3ba1f6ff265
Danilo Assunção <35316786+danilo-assuncao@users.noreply.github.com>
2022-11-15 18:13:58 +0000

    test mergesort algorithm

--- TwoSum.java
+++ TwoSum.java
@@ -1,11 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class TwoSum {
     
     public static void main(String[] args) {
-        twoSum(new int[] {1, 2}, 3);
+        twoSum(new int[] {5, 4, 1, 2}, 3);
     }
 
     public static int[] twoSum(int[] nums, int target) {
-
+        Arrays.sort(nums);
+        Arrays.stream(nums).boxed().toList().forEach(it -> System.out.println(it));
         return new int[] {1, 2};
     }
 }
