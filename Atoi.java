835ff4f32e4b9390e5988e61c7fd3ab31e828c75
Danilo Assunção <35316786+danilo-assuncao@users.noreply.github.com>
2022-11-15 16:41:43 +0000

    apply improvements in atoi exercise

--- Atoi.java
+++ Atoi.java
@@ -1,19 +1,18 @@
 class Atoi {
 
     public static void main(String[] args) {
-        myAtoi("   4540   ");
+        myAtoi("-0032");
     }
 
     public static int myAtoi(String s) {
         var number = 0;
+        var exponent = 0;
         var signal = '+';
-        var numberCharLength = s.length() - 1;
-        for (int i = numberCharLength; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             final var digit = s.charAt(i);
             if (digit >= 48 && digit <= 57) {
-                number += (digit - '0') * Math.pow(10, numberCharLength - i);
-            } else if (digit == 32) {
-                continue;
+                number += (digit - 48) * Math.pow(10, exponent);
+                exponent++;
             } else if (digit == 43 || digit == 45) {
                 signal = digit;
             }
