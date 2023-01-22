9ef516376888c05b952d8a9e108fdbc1d4544c09
Danilo Assunção <35316786+danilo-assuncao@users.noreply.github.com>
2023-01-22 16:06:50 +0000

    add valid parentheses challenge code

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,3 +1,27 @@
+/** 
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+ * I can be placed before V (5) and X (10) to make 4 and 9. 
+ * X can be placed before L (50) and C (100) to make 40 and 90. 
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ 
+ * URL: https://leetcode.com/problems/roman-to-integer 
+*/
+
 import static java.util.Map.entry;
 
 import java.util.Map;
@@ -5,7 +29,8 @@ import java.util.Map;
 public class RomanToInteger {
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("IV")); // 4
+        System.out.println(romanToInt("VI")); // 6
     }
 
     private static final Map<Character, Integer> ROMAN_VALUES = 
