1b26eaa31454a39ee69bcee26771b05bd310a4f0
Danilo Assunção <35316786+danilo-assuncao@users.noreply.github.com>
2023-01-22 17:41:04 +0000

    add comment in palindrome number algorithm

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,4 +1,10 @@
+/**
+ * Given an integer x, return true if x is a palindrome, and false otherwise.
 
+ * An integer is a palindrome when it reads the same forward and backward. For example, 121 is a palindrome while 123 is not.
+
+ * URL: https://leetcode.com/problems/palindrome-number
+ */
 
 public class PalindromeNumber {
 
